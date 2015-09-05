630ea7191acb4b433e6fd14eff56b8320d1db77e
Dylan Krause <dykrause@gmail.com>
2015-09-05 12:14:39 -0400

    Add webpack with webpack config settings -load by default dev settings -use babel, html, and css loaders -output to build -add resolve option with js extensions

---
+++ config/webpack.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve("./", 'node_modules');
+var mainPath = path.resolve("./", "app","assets","javascripts", "main.js");
+
+module.exports = function(env){
+
+  var webpackConfig = {
+    entry:  mainPath,
+    output: {
+      path: __dirname,
+      filename: "build.js"
+    },
+    plugins: [],
+    module: {
+      loaders: [
+        { test: /\.html$/, loader: "html"},
+        { test: /\.css$/, loader: 'style!css'},
+        { test: /\.js$/, loader: 'babel-loader?stage=0', exclude: /node_modules/}
+      ]
+    },
+    resolve: {
+      extensions: ['','.js']
+    }
+  }
+
+  if (env === 'development') {
+    webpackConfig.devtool = "eval",
+    webpackConfig.debug = true
+  }
+
+  return webpackConfig
+}
