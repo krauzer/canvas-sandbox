c5e2bba2e3e8b46802ac74b97748aad0e134f6be
Dylan Krause <dykrause@gmail.com>
2015-09-07 23:01:15 -0400

    Add requestAnimation check and polyfill

---
+++ app/assets/javascripts/utilities/api.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const requestAnimationFrame = function(callback, element){
+
+            let requestAnimationFrame =
+                window.requestAnimationFrame        ||
+                window.webkitRequestAnimationFrame  ||
+                window.mozRequestAnimationFrame     ||
+                window.oRequestAnimationFrame       ||
+                window.msRequestAnimationFrame      ||
+                function(callback, element) {
+                    var currTime = new Date().getTime();
+                    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                    var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    }, timeToCall);
+                    lastTime = currTime + timeToCall;
+                    return id;
+                };
+
+            return requestAnimationFrame.call(window, callback, element);
+        }
+}
