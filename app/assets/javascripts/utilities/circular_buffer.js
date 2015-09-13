b8fe06d349f56b6fb334a81ca97ca35309f3f372
Dylan Krause <dykrause@gmail.com>
2015-09-13 17:32:46 -0400

    Add Circular Buffer class that will be used for logging better frame rate averages

---
+++ app/assets/javascripts/utilities/circular_buffer.js
@@ -0,0 +1,29 @@
+class CircularBuffer {
+
+  constructor(length){
+    this.buffer = new Int32Array(length);
+    this.maxSize = length;
+    this.currentSize = 0;
+    this.openIndex = 0;
+    this.lastFilledIndex = -1;
+  }
+
+  get(index){
+    return this.buffer[(this.lastFilledIndex+index)%this.maxSize];
+  }
+
+  size(){
+    return this.currentSize;
+  }
+
+  add(el){
+    if (this.currentSize< this.maxSize) this.currentSize++;
+    this.buffer[this.openIndex] = el;
+    this.lastFilledIndex = (this.lastFilledIndex+1)%this.maxSize;
+    this.openIndex = (this.lastFilledIndex + 1)%this.maxSize;
+  }
+
+
+}
+
+module.exports = CircularBuffer;
